e8363f9b56887eee28c9721e6366db8a33b563c3
Ayman Said <ayman-said@live.com>
2019-11-02 15:49:58 +0200

    Bootstrap RTL

--- cStone/main/urls.py
+++ cStone/main/urls.py
@@ -2,16 +2,24 @@ from django.urls import path
 from django.views.generic import TemplateView
 from . import views
 
+
+app_name = 'main'
+
 urlpatterns = [
+    path(
+        "",
+        TemplateView.as_view(template_name="home.html"),
+        name="home",
+    ),
     path(
         "about-us/",
         TemplateView.as_view(template_name="about_us.html"),
         name="about_us",
     ),
     path(
-        "",
-        TemplateView.as_view(template_name="home.html"),
-        name="home",
+        "new-quote/",
+        TemplateView.as_view(template_name="new_quote.html"),
+        name="new_quote",
     ),
     path(
         "contact-us/",
--- cStone/orders/views.py
+++ cStone/orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render
+
+from orders.models import Order, OrderItem
 
 # Create your views here.
+
+
+def new_quote(request):
+    new_order = Order.objects.create(status='quote')
+
+    # return render(request, r,
+    #               {'form': form, 'course': course})
